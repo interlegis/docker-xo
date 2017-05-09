7b8fbf03fbbd2fc5add59570c3674a0462b95213
Fabio Rauber <fabiorauber@gmail.com>
2017-05-09 11:13:01 -0300

    Changed separator for sed in start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ cd /home/node/xo-server
 
 # Replace Redis Server configuration
 if [ ! -z "$REDIS_ENV_REDIS_PASSWORD" ]; then
-  sed -i "s/#uri: ''/uri: 'redis:\/\/0:$REDIS_ENV_REDIS_PASSWORD@$REDIS_SERVER:$REDIS_PORT'/" .xo-server.yaml
+  sed -i "s|#uri: ''|uri: 'redis://0:$REDIS_ENV_REDIS_PASSWORD@$REDIS_SERVER:$REDIS_PORT'|" .xo-server.yaml
 else
-  sed -i "s/#uri: ''/uri: 'redis:\/\/$REDIS_SERVER:$REDIS_PORT'/" .xo-server.yaml
+  sed -i "s|#uri: ''|uri: 'redis://$REDIS_SERVER:$REDIS_PORT'|" .xo-server.yaml
 fi
 
 # storage directory and fix perms
